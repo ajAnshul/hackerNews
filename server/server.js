4987ba104e8f6f3506fd2848f68a847e64bf128c
Anshul Jain <anshuldpma@gmail.com>
2020-06-07 14:03:20 +0530

    server code changes

--- server/server.js
+++ server/server.js
@@ -2,19 +2,29 @@ import express from "express";
 import fs from "fs";
 import path from "path";
 
+
 import React from "react";
 import ReactDOMServer from "react-dom/server";
-
 import App from "../src/App";
 
 const PORT = 5000;
 
-const app = express();
 
+
+const app = express();
 app.use(express.static(path.resolve(__dirname, '..', 'build')))
 
+app.get("/", (req, res)=>{
+  return renderHackerNews(req, res);
+})
 
-app.use("^/$", (req, res, next) => {
+app.get("/page/:pageNumber", (req, res) => {
+  return renderHackerNews(req, res);
+});
+
+
+
+function renderHackerNews(req, res){
   fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
     if (err) {
       console.log(err);
@@ -22,27 +32,8 @@ app.use("^/$", (req, res, next) => {
     }
     return res.send(data);
   });
-});
+}
 
-app.get("/", (req, res, next) => {
-  fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).send("Some error happened");
-    }
-    return res.send(data);
-  });
-});
-
-// app.get("/page/:pageNumber", (req, res, next) => {
-//     fs.readFile(path.resolve("../build/index.html"), "utf-8", (err, data) => {
-//       if (err) {
-//         console.log(err);
-//         return res.status(500).send("Some error happened");
-//       }
-//       return res.send(data);
-//     });
-//   });
 
 
 app.listen(process.env.PORT || PORT, () => {
